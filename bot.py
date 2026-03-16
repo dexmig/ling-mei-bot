bedcb7638e914b610f418ed54d735c08ee78106b
dexmig <dexmig@users.noreply.github.com>
2026-03-16 11:08:09 +0200

    update bot

--- bot.py
+++ bot.py
@@ -180,8 +180,9 @@ async def main():
     dp.message.register(back_handler, F.text == "⬅ Назад")
     dp.message.register(order_handler, F.text == "🛒 Оформити замовлення")
 
+    dp.message.register(quantity_handler, F.text.regexp(r"^\d+$"))
     dp.message.register(product_handler, F.text)
-    dp.message.register(quantity_handler, F.text)
+    
 
 
 
